b14038430a9abdd8003966e0e6bbabbd0dde7656
rafal A <rafalandrzejczuk1@gmail.com>
2023-02-12 17:02:37 +0100

    1 commit

---
+++ js/script.js
@@ -0,0 +1,41 @@
+
+let btn1 = document.getElementById('get-data');
+console.log(btn1);
+
+
+const getData1 = () => {
+    fetch('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php')
+        .then(res => res.json())
+        .then(data => {
+
+            console.log(data);
+            $(document).ready(function () {
+                $('div').attr('id', 'dane-programisty')
+            })
+
+            let pImie = document.createElement('div');
+            let nazwisko = document.createElement('div');
+            let pZawod = document.createElement('div');
+            let pFirma = document.createElement('div');
+            let hr = document.createElement('hr')
+
+
+            pImie.innerText = `Imie: ${data.imie} `;
+            nazwisko.innerText = `Nazwisko: ${data.Nazwisko} `;
+            pZawod.innerText = `Zawód: ${data.zawod} `;
+            pFirma.innerText = `Firma: ${data.firma} `;
+
+            document.body.appendChild(pImie);
+            document.body.appendChild(nazwisko);
+            document.body.appendChild(pZawod);
+            document.body.appendChild(pFirma);
+            document.body.appendChild(hr)
+        })
+        .catch(error => {
+            console.error(error);
+        })
+}
+
+
+btn1.addEventListener('click', getData1)
+
